b918914ac9971abcdfbe3a2b9896eae7be747346
Ovyerus <iamovyerus@gmail.com>
2018-02-10 21:02:40 +1100

    Revert "Rewrite to another lib again. NOT WORKING"
    This reverts commit 4a65c7fcca00596bd809095886453db14ee8c5c5.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is the next chatbot engine for Monika, replacing the Dialogflow implementation on v1.  
 It's a simple Tensorflow chatbot capable of learning from user context.
 
-The chatbot is a version of [Conchylicultor/DeepQA](https://github.com/Conchylicultor/DeepQA) that is modified to run off YAML config files and interface with a HTTP API.
+The chatbot is a Python 3.6 port, and extension of [tensorlayer/seq2seq-chatobbt](https://github.com/tensorlayer/seq2seq-chatbot) that is modified to run off YAML config files and interface with a HTTP API.
 
 # Dependencies
 
@@ -16,5 +16,5 @@ The chatbot is a version of [Conchylicultor/DeepQA](https://github.com/Conchylic
 
 # Licenses
 
-Contains code from "Conchylicultor", licensed under the Apache License 2.0, included with each file from the project.  
+Contains code from "zsdonghao", license unspecified.  
 All code not from the forked project is licensed under the MIT license.
--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,42 @@
+import os.path as osp
+
+import os
+import importlib
+
+
+def load_data_module(path):
+    '''
+    Loads a data module at the given path.
+    Can actually be used to load any module, but was designed for loading data modules from
+    the variables in the config, easier.
+    '''
+    if not osp.exists(path):
+        raise ValueError(f'path "{path}" does not exist.')
+
+    path = path.replace('/', '.').replace('\\', '.') + '.data'
+    module = importlib.import_module(path)
+
+    return module
+
+
+def get_latest_trained_data(path):
+    '''
+    Returns a list of the latest trained data in a directory, descending by time.
+    '''
+    if not osp.exists(path):
+        raise ValueError(f'path "{path}" does not exist.')
+
+    files = [x for x in os.scandir(path) if x.is_file() and x.name.endswith('.npz')]
+    times = [x.stat().st_mtime for x in files]
+    pairs = sorted(list(zip([f'{path}/{x.name}' for x in files], times)), key=lambda x: x[1], reverse=True)
+
+    return pairs
+
+
 class AttrObj:
-    """
-    Object that takes in a dict, and converts it to a dot format thing.
-    Makes accessing the config a tad easier.
-    """
-    def __init__(self, d: dict):
+    def __init__(self, d):
         for k, v in d.items():
-            # Recursively convert child dicts to AttrObjs.
             if type(v) == dict:
                 d[k] = AttrObj(v)
 
         self.__dict__ = d
-
-    def to_dict(self):
-        """
-        Transforms the AttrObj back into a dictionary, including all nested AttrObjs.
-        """
-        res = {}
-
-        for k, v in self.__dict__.items():
-            if isinstance(v, AttrObj):
-                res[k] = v.to_dict()
-            else:
-                res[k] = v
-
-        return res
