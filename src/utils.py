f6b057d6784a81d58d3a838407c0a207456185de
Ovyerus <iamovyerus@gmail.com>
2018-02-10 21:48:25 +1100

    Re-add improved AttrObj

--- src/utils.py
+++ src/utils.py
@@ -34,9 +34,28 @@ def get_latest_trained_data(path):
 
 
 class AttrObj:
-    def __init__(self, d):
+    """
+    Object that takes in a dict, and converts it to a dot format thing.
+    Makes accessing the config a tad easier.
+    """
+    def __init__(self, d: dict):
         for k, v in d.items():
+            # Recursively convert child dicts to AttrObjs.
             if type(v) == dict:
                 d[k] = AttrObj(v)
 
         self.__dict__ = d
+
+    def to_dict(self):
+        """
+        Transforms the AttrObj back into a dictionary, including all nested AttrObjs.
+        """
+        res = {}
+
+        for k, v in self.__dict__.items():
+            if isinstance(v, AttrObj):
+                res[k] = v.to_dict()
+            else:
+                res[k] = v
+
+        return res
