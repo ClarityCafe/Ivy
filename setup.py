ce32a1599ae9ae1e84f0dc71484d9e73cc166182
Capuccino <enra@sayonika.moe>
2018-11-05 07:02:37 +0000

    make setup.sh executable

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+if "CI" in os.environ:
+    import sys
+    sys.path.insert(0, "ChatLearner/chatbot/")
+    
+    from bottrainer import BotTrainer
+
 from ChatLearner.chatbot.bottrainer import BotTrainer
 
 corpus_dir = "resources/chatbot/corpus"
